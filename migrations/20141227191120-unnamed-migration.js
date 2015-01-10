3ceef16655c43db390da5cbb9e586087199f58af
Henrik Lievonen <henrik.lievonen@hotmail.com>
2015-01-10 10:57:28 +0200

    Fixes migration

--- migrations/20141227191120-unnamed-migration.js
+++ migrations/20141227191120-unnamed-migration.js
@@ -3,13 +3,11 @@
 module.exports = {
     up: function (migration, DataTypes, done) {
         // add altering commands here, calling 'done' when finished
-        migration.addColumn('movies', 'origname', DataTypes.STRING);
-        done();
+        migration.addColumn('Movies', 'origname', DataTypes.STRING).done(done());
     },
     
     down: function (migration, DataTypes, done) {
         // add reverting commands here, calling 'done' when finished
-        migration.removeColumn('movies', 'origname');
-        done();
+        migration.removeColumn('Movies', 'origname').done(done());
     }
 };
